bdf01ddd101f1f42577c4f305c5a3b4d296670b6
Henrique Troyack <henriquetroyack@ugb.edu.br>
2018-07-12 20:51:52 -0300

    who else loves git?

---
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    //public class HelloWorldController : Controller
+    //{
+    //    // GET: /<controller>/
+    //    public IActionResult Index()
+    //    {
+    //        return View();
+    //    }
+    //}
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        /*
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        */
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /*
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+        */
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        // public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
+        {
+            // return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
