8956a2bbc6dc8434aedcea87c10ccc7cda7ae4ee
Henrique Troyack <henriquetroyack@ugb.edu.br>
2018-07-24 19:08:41 -0300

    The same thing we do every night, Pinky - try to take over the world!

---
+++ MvcMovie/MvcMovie/Controllers/VersionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class VersionController : Controller
+    {
+        public string Index()
+        {
+            // typeof(Controller).Assembly.GetName().Version.ToString()
+            string version = typeof(Controller).Assembly.GetName();
+            version += version.Version.ToString();
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+        }
+    }
+}
